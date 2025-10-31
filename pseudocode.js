bf32e44632008e5cab9c08585e907d54009ff983
rubenelhore <ruben.elhore@gmail.com>
2025-10-31 13:47:27 -0600

    Added pseudocode file

---
+++ pseudocode.js
@@ -0,0 +1,55 @@
+// Getting familiar to writing PSEUDOCODE
+
+ //a function that returns the sum of two numbers
+
+ // START
+ // GET First number
+ // GET second number
+ // RETURN SUM first number + second number
+ // END
+
+
+
+ // a function that takes an array of strings, and returns a string that is all those strings concatenated together
+ // 
+ // START
+ // SET array of strings -- set them already
+ // SET string -- set an empty string to add the elements in the array
+ // function
+ //     FOR i IN "ARRAY" -- we go one element by one element of the array
+ //     string.add(i) -- we add this elemements to the string
+ //return string - return the string
+ // 
+
+
+ // a method that takes an array of integers, and returns a new array with every other element from the original array, starting with the first element. 
+ //
+ // SET the array of numbers
+ // FOR to pass in each element
+ // WHEN the index key is % 2 === 0
+ // ADD to an empty array
+ // RETURN the array
+
+
+ //a function that determines the index of the 3rd occurrence of a given character in a string. For instance, if the given character is 'x' and the string is 'axbxcdxex', the function should return 6 (the index of the 3rd 'x'). If the given character does not occur at least 3 times, return null.
+ // GET the character
+ //     FOR to compare each character in the string 
+ //         Counter to count each time a character appears === to character
+ //         IF count === 3
+ //             GET INDEX which is === i
+ //             RETURN INDEX
+ //     return null
+ 
+
+
+ //a function that takes two arrays of numbers and returns the result of merging the arrays. The elements of the first array should become the elements at the even indexes of the returned array, while the elements of the second array should become the elements at the odd indexes. 
+ // GET Array1
+ // GET Array2
+ // SET Var []
+ // FUNCTION MERGE (Array1, Array2
+ //.    for i in Array1
+ //       Var.add(Array1[i])
+ //.      Var.add(Array2[i])
+ //RETURN Var
+ //
+
